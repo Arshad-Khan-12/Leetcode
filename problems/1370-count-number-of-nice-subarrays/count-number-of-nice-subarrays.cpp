0e1d036f46f3f630129e696a261890bef95a35d6
Arshad Khan H <121614052+Arshad-Khan-12@users.noreply.github.com>
2025-01-07 21:23:47 +0530

    Time: 8 ms (58.93%) | Memory: 71.4 MB (76.84%) - LeetSync

--- problems/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ problems/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> count;  // To store the frequency of prefix sums
+        return atMost(nums, k) - atMost(nums, k - 1);
+
+
+
+        // unordered_map<int, int> count;  // To store the frequency of prefix sums
         
-        int curr_sum = 0;  // Current prefix sum
-        int total_subarrays = 0;  // Total number of valid subarrays
+        // int curr_sum = 0;  // Current prefix sum
+        // int total_subarrays = 0;  // Total number of valid subarrays
 
-        // Iterate through the array
-        for (int num : nums) {
-            curr_sum += num&1;  // Update the current prefix sum
-            if(curr_sum == k){
-                total_subarrays++;
-            }
-            // Check if there is a prefix sum such that curr_sum - k exists in the map
-            if (count.find(curr_sum - k) != count.end()) {
-                total_subarrays += count[curr_sum - k];  // Add the number of such prefix sums
-            }
+        // // Iterate through the array
+        // for (int num : nums) {
+        //     curr_sum += num&1;  // Update the current prefix sum
+        //     if(curr_sum == k){
+        //         total_subarrays++;
+        //     }
+        //     // Check if there is a prefix sum such that curr_sum - k exists in the map
+        //     if (count.find(curr_sum - k) != count.end()) {
+        //         total_subarrays += count[curr_sum - k];  // Add the number of such prefix sums
+        //     }
 
-            // Increment the frequency of the current prefix sum
-            count[curr_sum]++;
+        //     // Increment the frequency of the current prefix sum
+        //     count[curr_sum]++;
+        // }
+
+        // return total_subarrays;  // Return the total number of subarrays with the given sum
+    }
+
+
+
+
+    
+    int atMost(vector<int>& nums, int goal) {
+        int right = 0;
+        int left = 0;
+        int sum = 0;
+        int result = 0;
+        for (right = 0; right < nums.size(); right++) {
+            sum += nums[right]& 1;
+            while (sum > goal && left <= right) { // finding ans for (sum<=goal) and then
+                sum -= (nums[left]&1);                //(sum<goal) i.e (sum<=goal-1) and then
+                left++;                           // subtracting both answeres will fetch the 
+                                                    // real ans.
+            }
+            result += right - left + 1;
         }
-
-        return total_subarrays;  // Return the total number of subarrays with the given sum
+        return result;
     }
 };
