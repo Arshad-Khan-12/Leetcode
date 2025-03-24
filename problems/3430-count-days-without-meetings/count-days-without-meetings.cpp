c1f7933c2c0f0e3c82677394e537aa5588d845f7
Arshad Khan H <121614052+Arshad-Khan-12@users.noreply.github.com>
2025-03-24 13:24:09 +0530

    Time: 71 ms (53.06%) | Memory: 126.5 MB (80.10%) - LeetSync

--- problems/3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ problems/3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
+        // Sort the meetings based on their start times.
+        sort(meetings.begin(), meetings.end());
         
-        sort(meetings.begin(),meetings.end());
-        vector<vector<int>>m2;
+        // Merge overlapping meetings.
+        vector<vector<int>> m2;
         m2.push_back(meetings[0]);
-
-        
-
-        for(int i=1;i<meetings.size();i++){
-            if(meetings[i][0] <= m2.back()[1] ){
-                m2.back()[1] = max(m2.back()[1],meetings[i][1]);
-            }
-            else{
+        for (int i = 1; i < meetings.size(); i++) {
+            // If the current meeting overlaps with the last merged meeting, update the end time.
+            if (meetings[i][0] <= m2.back()[1]) {
+                m2.back()[1] = max(m2.back()[1], meetings[i][1]);
+            } else {
+                // Otherwise, add this meeting as a new interval.
                 m2.push_back(meetings[i]);
             }
         }
-
-
-        int ans=0;
-        ans+=m2[0][0]-1;
-        for(int i=1;i<m2.size();i++){
-            ans += (m2[i][0]-m2[i-1][1]-1);
+        
+        int ans = 0;
+        // Count free days before the first meeting.
+        ans += m2[0][0] - 1;
+        
+        // Count free days between merged meetings.
+        for (int i = 1; i < m2.size(); i++) {
+            ans += (m2[i][0] - m2[i - 1][1] - 1);
         }
-
-        ans += (days-m2.back()[1]);
-
+        
+        // Count free days after the last meeting.
+        ans += (days - m2.back()[1]);
+        
         return ans;
-
-
-
     }
 };
